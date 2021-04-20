a8acaf8e0fd71145f78e358f5796f082303287cf
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-04-20 01:36:33 -0300

    primeiro teste

---
+++ 1-primeiroProgama.js
@@ -0,0 +1,10 @@
+console.log("Meu Primeiro Progama: trabalhando com variaveis");
+
+const idade = 22;
+
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade + 2;
+
+console.log(idadeSomada);
