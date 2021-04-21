b70392b647873de9d31cd58a67099fd30e867609
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-04-21 18:55:34 -0300

    adadsdasdssa

---
+++ 5-atribuicaodeVariaveis.js
@@ -0,0 +1,26 @@
+console.log("Trabalhando com atribuição de variaveis");
+
+const primeiroNome = "João "
+const sobreNome = "Baptista"
+
+//console.log(idade + " " + sobreNome);
+console.log(primeiroNome, sobreNome);
+
+
+
+console.log(`Meu nome é ${primeiroNome} ${sobreNome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobreNome;
+console.log(`Meu nome é ${nomeCompleto}`);
+
+let idade; //declarando variavel
+idade = 22; //atribuindo valor
+idade = idade+1;
+console.log(idade);
+
+
+
+console.log(`Meu nome é ${primeiroNome}. ` + `Meu sobrenome é ${sobreNome}. ` + ` E minha idade é ${idade}.` );
---
+++ 6-listas.js
@@ -0,0 +1,13 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+console.log("Destinos possíveis: ");
+console.log(listaDestinos); 
