4f6d1b0c2109cf6958f8a5d932c62a1408be1074
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-04-21 17:26:46 -0300

    a

---
+++ 4-conversaoDetipos.js
@@ -0,0 +1,11 @@
+console.log("Conversao de tipos");
+
+console.log("ano " + 2021);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log ("10" / "2");
+console.log ("JAO" / "2"); //Not a Number NaN
+
+console.log(6.5);
+console.log(6,5);
