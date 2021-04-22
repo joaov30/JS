17149647183f17b36ed24dd2428b6fe7dbd555f5
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-04-21 22:49:08 -0300

    Create 7-condicionais.js

---
+++ 7-condicionais.js
@@ -0,0 +1,19 @@
+console.log(`Trabalhando com condicionais`);
+const listaDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+    
+);
+
+
+
+console.log("Destinos possíveis: ");
+console.log(listaDestinos); 
+
+listaDestinos.splice(1,1); //removendo item
+
+console.log(listaDestinos);
+
+
+
