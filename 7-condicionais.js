fe7229363215de8dfbbc8d874c6febf709ab079a
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-04-23 17:31:03 -0300

    Update 7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -3,17 +3,38 @@ const listaDestinos = new Array(
     `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
-    
+
 );
 
 
-
+const idadeComprador = 17;
+const estaAcompanhada = true;
 console.log("Destinos possíveis: ");
-console.log(listaDestinos); 
+console.log(listaDestinos);
 
-listaDestinos.splice(1,1); //removendo item
+if (idadeComprador >= 18) {
+
+    console.log("Comprador maior de Idade");
+    listaDestinos.splice(1, 1); //removendo item
+
+} else if (estaAcompanhada) {
+
+    console.log("Comprador esta acompanhado");
+    listaDestinos.splice(1, 1); //removendo item
+
+} else {
+
+    console.log("Comprador é menor de idade não será possivel realizar a compra");
+
+}
 
 console.log(listaDestinos);
+
+//console.log(idadeComprador > 18);
+//console.log(idadeComprador < 18);
+//console.log(idadeComprador <= 18);
+//console.log(idadeComprador >= 18);
+//console.log(idadeComprador == 18);
 
 
 
